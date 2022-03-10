0c185791c16b4a3982f6d73a03f2aa50bc0aee23
Dung Le <kimpdung47@gmail.com>
2022-03-10 00:38:09 -0800

    changed dataset used + fixed test_mean.py accordingly

--- tests/test_mean.py
+++ tests/test_mean.py
@@ -1,6 +1,5 @@
 import mean_cross_val
 import pandas as pd
-import pytest
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
@@ -10,22 +9,29 @@ from sklearn.neighbors import KNeighborsClassifier
 class Data:
 
     def __init__(self):
-        col_name = ['brand.name', 'tar.content', 
-                    'nicotine.content', 'weight', 'carbon.monoxide.content']
-        ds = pd.read_csv("cigarettes_c.txt", names=col_name, sep=",")
-        ds = ds.drop(columns=["brand.name"])
-        train_df, test_df = train_test_split(ds, test_size=0.3, random_state=123)
-        self.X_train = train_df.drop(columns=["carbon.monoxide.content"])
-        self.X_test = test_df.drop(columns=["carbon.monoxide.content"])
-        self.y_train = train_df["carbon.monoxide.content"]
-        self.y_test = test_df["carbon.monoxide.content"]
+        col_names = ["id", "clump", "unif_size",
+                     "unif_shape", "adhesion", "epi_size", "nuclei",
+                     "chromatin", "nucleoli", "mitoses", "class"]
+        dataset = pd.read_csv("breast_cancer_1.txt",
+                              names=col_names, sep=",")
+        dataset = dataset[(dataset != '?').all(axis=1)]
+        dataset['nuclei'] = dataset['nuclei'].astype(int)
+        dataset = dataset.drop(columns=["id"])
+        dataset['class'] = dataset['class'].replace([2], 0)
+        dataset['class'] = dataset['class'].replace([4], 1)
+        train_df, test_df = train_test_split(dataset,
+                                             test_size=0.3, random_state=123)
+        self.X_train = train_df.drop(columns=["class"])
+        self.X_test = test_df.drop(columns=["class"])
+        self.y_train = train_df["class"]
+        self.y_test = test_df["class"]
 
 
-def test_mean_cross_val_correct():
+def test_mean_cross_val_correct_simple():
     """mean_cross_val_scores returns correct mean
     of cross validation with correct types of inputs""" 
     """test if it is a series"""
-    """test if num of elements are the same"""
+    """test if num of elements are as expected"""
     """test if elements bigger than 0 and smaller than 1"""
     dataset = Data()
     scale = StandardScaler()
@@ -35,7 +41,27 @@ def test_mean_cross_val_correct():
     result = mean_cross_val.mean_cross_val_scores(
         pipe_knn, dataset.X_train, dataset.y_train,
         return_train_score=True, scoring=scoring)
-    assert result[1] < 1
-    assert result[1] >= 0
-    assert isinstance(result, series)
+    assert all(result) <= 1
+    assert all(result) >= 0
+    assert result.count() == 4
+    assert isinstance(result, pd.core.series.Series)
 
+
+def test_mean_cross_val_correct():
+    """mean_cross_val_scores returns correct mean
+    of cross validation with correct types of inputs""" 
+    """test if it is a series"""
+    """test if num of elements are as expected"""
+    """test if elements bigger than 0 and smaller than 1"""
+    dataset = Data()
+    scale = StandardScaler()
+    pipe_knn = make_pipeline(scale,
+                             KNeighborsClassifier(n_neighbors=5))
+    scoring = ['accuracy', "f1", "recall", "precision"]
+    result = mean_cross_val.mean_cross_val_scores(
+        pipe_knn, dataset.X_train, dataset.y_train,
+        return_train_score=True, scoring=scoring)
+    assert all(result) <= 1
+    assert all(result) >= 0
+    assert result.count() == 10
+    assert isinstance(result, pd.core.series.Series)
