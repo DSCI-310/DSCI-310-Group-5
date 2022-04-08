6580aa224ff9dc60a8e304f8058e7f8ad9141454
Dung Le <kimpdung47@gmail.com>
2022-04-07 18:13:25 -0700

    added exception error and tests for plot_cm

--- src/plot_confusion_matrix.py
+++ src/plot_confusion_matrix.py
@@ -2,10 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.metrics import (confusion_matrix, ConfusionMatrixDisplay)
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
 
 
 def plot_cm(model, X_train, y_train, X_test, y_test, title):
@@ -13,23 +9,42 @@ def plot_cm(model, X_train, y_train, X_test, y_test, title):
     Returns confusion matrix on predictions of y_test with given title 
     of given model fitted X_train and y_train 
     -----------
-    Parameters
-    -----------
+    PARAMETERS:
     model :
-        scikit-learn model
-    X_train : numpy array or pandas DataFrame
+        scikit-learn model or sklearn.pipeline.Pipeline
+    X_train : numpy array or pandas DataFrame/Series
         X in the training data
-    y_train : numpy array or pandas DataFrame
+    y_train : numpy array or pandas DataFrame/Series
         y in the training data
-    X_test : numpy array or pandas DataFrame
+    X_test : numpy array or pandas DataFrame/Series
         X in the testing data
-    y_test : numpy array or pandas DataFrame
+    y_test : numpy array or pandas DataFrame/Series
         y in the testing data
-    Returns
     -----------
-        A sklearn.metrics._plot.confusion_matrix.ConfusionMatrixDisplay object 
-    """
+    REQUISITES:
+    X_train, y_train, X_test, y_test cannot be empty.
+    -----------
+    RETURNS:
+    A sklearn.metrics._plot.confusion_matrix.ConfusionMatrixDisplay object 
+    -----------
+    Examples
 
+    plot_cm(DecisionTreeClassifier(), X_train, y_train, X_test, y_test, "Fig")
+    """
+    if not isinstance(X_train, (pd.core.series.Series,
+                                pd.core.frame.DataFrame, np.ndarray)):
+        raise TypeError("'X_train' should be of type numpy.array or pandas.Dataframe")
+    if not isinstance(y_train, (pd.core.series.Series,
+                                pd.core.frame.DataFrame, np.ndarray)):
+        raise TypeError("'y_train' should be of type numpy.array or pandas.Dataframe")
+    if not isinstance(X_test, (pd.core.series.Series,
+                               pd.core.frame.DataFrame, np.ndarray)):
+        raise TypeError("'X_test' should be of type numpy.array or pandas.Dataframe")
+    if not isinstance(y_test, (pd.core.series.Series,
+                               pd.core.frame.DataFrame, np.ndarray)):
+        raise TypeError("'y_test' should be of type numpy.array or pandas.Dataframe")
+    if not isinstance(title, str):
+        raise TypeError("'title' should be of 'str'")
     model.fit(X_train, y_train)
     predictions = model.predict(X_test)
     cm = confusion_matrix(y_test, predictions, labels=model.classes_)
